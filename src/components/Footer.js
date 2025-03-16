35456824ab2ab6740b9b6125ba30a09e73ec75e3
Anup Ghattikar <aghattikar82@gmail.com>
2025-03-16 23:49:48 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer() {
       <footer className="bg-gray-900 text-white p-6 text-center mt-8">
         <p>© 2025 Anup Ghattikar</p>
         <ul className="flex justify-center space-x-4 mt-4">
-          <li><a href="your_github_link" target="_blank" className="hover:text-gray-400">GitHub</a></li>
-          <li><a href="your_linkedin_link" target="_blank" className="hover:text-gray-400">LinkedIn</a></li>
+          <li><a href="https://github.com/aghattikar82" target="_blank" className="hover:text-gray-400">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/aghattikar82/" target="_blank" className="hover:text-gray-400">LinkedIn</a></li>
         </ul>
       </footer>
     );
